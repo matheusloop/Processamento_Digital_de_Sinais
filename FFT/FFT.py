6d26146a61e07c43db9679b4546e5a8ff480a550
matheusloop <marcosraila@gmail.com>
2025-07-28 19:24:10 -0300

    Implementação da IFFT
    Implementação da transformada inversa da FFT

--- FFT/FFT.py
+++ FFT/FFT.py
@@ -8,7 +8,6 @@ def W_N(N):
 
 # Faz o zero padding do sinal para a potência de 2 mais próxima ou para o valor de N
 def zero_padding(signal, N=None):
-
   if N == None:
     l = np.ceil(np.log2(len(signal)))
     N = int(2**l)
@@ -19,34 +18,64 @@ def zero_padding(signal, N=None):
   return signal_zp
 
 
-#Faz o cálculo recursivo da FFT
+# Faz o cálculo recursivo da FFT
 def calculate_fft(signal):
-  N = len(signal) #Determina o valor de N
+  N = len(signal) # Determina o valor de N
 
-  #Se o tamanho for unitário, retorna o próprio valor
+  # Se o tamanho for unitário, retorna o próprio valor
   if N == 1:
     return signal
 
-  #Para dimenção maior, é feita a divisão da DFT em duas e
+  # Para dimensão maior, é feita a divisão da DFT em duas
   if N > 1:
-
     X_e = calculate_fft(signal[::2])
     X_o = calculate_fft(signal[1::2])
-    #Calcula a parte superior da DFT usando a soma das partes pares e impares ponderadas
+
+    # Calcula a parte superior da DFT usando a soma das partes pares e ímpares ponderadas
     X_up   = X_e + W_N(N) * X_o
 
-    #Calcula a parte inferior da DFT usando a subtração das partes pares e impares ponderadas
+    # Calcula a parte inferior da DFT usando a subtração das partes pares e ímpares ponderadas
     X_down = X_e - W_N(N) * X_o
 
-  #Retorna a DFT concatenada
+  # Retorna a DFT concatenada
   return np.concatenate([X_up, X_down])
 
 
-#Calcula a FFT ajustando o numero de amostras
+# Calcula a FFT ajustando o número de amostras
 def myFFT(signal, N = None):
-
-  #Faz o zero padding no sinal
+  # Faz o zero padding no sinal
   signal_zp = zero_padding(signal, N)
 
-  #Retorna a FFT do sinal
+  # Retorna a FFT do sinal
   return calculate_fft(signal_zp)
+
+
+# Faz o cálculo recursivo da IFFT
+def calculate_ifft(espectrum):
+  N = len(espectrum) # Determina o valor de N
+
+  # Se o tamanho for unitário, retorna o próprio valor
+  if N == 1:
+    return espectrum
+
+  # Para dimensão maior, é feita a divisão da DFT em duas
+  if N > 1:
+    X_e = calculate_ifft(espectrum[::2])
+    X_o = calculate_ifft(espectrum[1::2])
+
+    # Calcula a parte superior da IDFT usando a soma das partes pares e ímpares ponderadas
+    x_up   = X_e + np.conj(W_N(N)) * X_o
+
+    # Calcula a parte inferior da IDFT usando a subtração das partes pares e ímpares ponderadas
+    x_down = X_e - np.conj(W_N(N)) * X_o
+
+  # Retorna a IDFT concatenada
+  return np.concatenate([x_up, x_down])
+
+
+# Calcula a IFFT
+def myIFFT(espectrum):
+  N = len(espectrum)
+
+  # Retorna a IFFT do espectro
+  return calculate_ifft(espectrum).real / N
